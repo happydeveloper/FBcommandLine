253e47fb0f8148c9ef27c34574f1390b14b03cbc
DURU <enfn2001@gmail.com>
2013-11-11 00:10:44 +0900

    공통코드추가

--- common/nav.php
+++ common/nav.php
@@ -1,4 +1,10 @@
-<?php
-//공통으로 들어가는 네비
 
-?>
+<nav id="main_menu">
+        <div class="wrapper">
+                <ul>
+                        <li><a href="./ot.php">생활코딩 페이스북 그룹</a></li>
+                        <li><a href="./GraphAPI.php">페이스북 FQL 테스트 페이지</a></li>
+                        <li><a href="./index.php">내 페이스북 친구들 얼굴들</a></li>
+                </ul>
+        </div>
+</nav>
