35053a3075661f29c1357ee01e0fe9d09d68e562
DURU <enfn2001@gmail.com>
2013-11-15 10:10:36 +0900

    nav 링크 수정

--- include/nav.php
+++ include/nav.php
@@ -2,10 +2,11 @@
 <nav id="main_menu">
         <div class="wrapper">
                 <ul>
-                        <li><a href="./ot.php">생활코딩 페이스북 그룹</a></li>
-                        <li><a href="./GraphAPI.php">페이스북 FQL 테스트 페이지</a></li>
-			<li><a href="./ot_comment.php">댓글보기</a></li>
                         <li><a href="./index.php">내 페이스북 친구들 얼굴들</a></li>
+                        <li><a href="./ot_stream.php">생활코딩 페이스북 그룹</a></li>
+			<li><a href="./ot_comment.php">댓글보기</a></li>
+
+                        <li><a href="./GraphAPI.php">페이스북 FQL 테스트 페이지</a></li>
                 </ul>
         </div>
 </nav>
