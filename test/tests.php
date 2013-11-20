f7698db3c6320fd56044eb8417a6a526501cf1fe
DURU <enfn2001@gmail.com>
2013-11-20 09:10:00 +0900

    테스트 코드 추가-  사용자의 상태를 가져오는 - 하루 한줄 코딩

---
+++ test/tests.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class ot_streamTestCase extends PHPUnit_Framework_TestCase 
+{
+	
+	public function testConstructor() {
+
+		$_SERVER['HTTP_HOST'] = 'ucloud.duru.pe.kr';
+    		$_SERVER['REQUEST_URI'] = '/ot_stream.php';
+    		$ot_stream = new ot_stream();
+		$this->assertNotEmpty($ot_stream);
+
+		return $ot_stream;
+  	}
+
+	/**
+	* @depends testConstructor
+	*/	
+	public function testGetUserState(ot_stream $stream){
+		 $_SERVER['HTTP_HOST'] = 'ucloud.duru.pe.kr';
+    		 $_SERVER['REQUEST_URI'] = '/ot_stream.php';
+    		 $login_url = parse_url($stream->getUserState());
+    		 $this->assertEquals($login_url['scheme'], 'https');
+    		 $this->assertEquals($login_url['host'], 'www.facebook.com');
+    		 $this->assertEquals($login_url['path'], '/dialog/oauth');
+	}
+}
---
+++ tests.sh
@@ -0,0 +1,1 @@
+ vendor/phpunit/phpunit/phpunit.php --stderr --bootstrap test/bootstrap.php test/tests.php
