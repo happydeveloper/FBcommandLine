fe86f245b340918a5a38ee6e85e1b6a607e515f4
DURU <enfn2001@gmail.com>
2013-11-26 10:36:24 +0900

    클래스 타입으로 변경-HTML과 class분리

--- ot_stream.php
+++ ot_stream.php
@@ -6,62 +6,6 @@ if (($loader = require_once __DIR__ . '/vendor/autoload.php') == null)  {
   die('Vendor directory not found, Please run composer install.');
 }
 
-class Ot_stream
-{
-	//페이스북 객체 
-	public $facebook;
-
-	public $user;
-
-	public $fql;
-
-	public $result;
-
-	public function __construct() 
-	{
-
-	$this->facebook = new Facebook(array(
-		'appId' => '541305629256667',
-		'secret' => '95492b0183156cd27d69b1308980ef26',
-		'cookie' => true));
-
-	$this->user = $this->facebook->getUser();
-	
-		if($this->user) 
-		{
-
-			try {
-				$this->user_profile = $this->facebook->api('/me');
-			} catch(FacebookApiException $e) {
-				error_log($e);
-				$this->user = null;
-			}
-		}
-	}
-
-	public function getUserState(){
-		if($this->user) {
-			return $logoutUrl = $this->facebook->getLogoutUrl();
-		} else {
-			return $loginUrl = $this->facebook->getLoginUrl();
-		}
-	}
-
-	public function getStream($startDate, $endDate)
-	{
-		$startDateType = new Datetime($startDate);
-		$endDateType   = new Datetime($endDate);
-
-		if($this->user) {
-			$this->fql = "SELECT post_id, created_time, permalink, message FROM stream WHERE source_id = 174499879257223 AND created_time < ".$endDateType->format('U')." AND created_time >= ".$startDateType->format('U')." LIMIT 50";
-		
-		$params = array('method' => 'fql.query', 'query' => $this->fql, );
-			echo "<br /> ".$this->fql;
-			$this->result =  $this->facebook->api($params);
-		}
-	}
-}
-
 ?>	
 <head>
     <meta charset="utf-8" />
@@ -72,6 +16,7 @@ class Ot_stream
 
 <?php
 //OnLoad 초기 로드시 작업
+	require_once 'classes/ot_stream.php';
 	include_once 'include/nav.php';
 $ot_stream = new Ot_stream();
 if($ot_stream->user) {
