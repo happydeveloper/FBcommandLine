42a85829a6f9d729b32c85c9321781d529617ff4
DURU <enfn2001@gmail.com>
2013-12-10 08:14:35 +0900

    php class study

--- classes/Inheritance.php
+++ classes/Inheritance.php
@@ -1,4 +1,4 @@
-<?php
+<?php #Inheritance.php
 
 class Person
 {
@@ -6,6 +6,14 @@ class Person
 
 	public function __construct($name = 'unknown', $surname = 'unknown')
 	{
+		$this->name = $name;
+		$this->surname = $surname;
+	} // end cunstructor
 
-	}
+	public function introduce()
+	{
+		echo "<p>Hello, my full name is <strong> 
+			$this->name $this->surname</strong>.</p>";
+	}// end introduce
 }
+/>
