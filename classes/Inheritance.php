aff5dd3e6b4c10ac652e0eca6c3fbb182e5cef5f
DURU <enfn2001@gmail.com>
2013-12-10 23:53:44 +0900

    상속 실습

--- classes/Inheritance.php
+++ classes/Inheritance.php
@@ -16,4 +16,27 @@ class Person
 			$this->name $this->surname</strong>.</p>";
 	}// end introduce
 }
-/>
+
+class Student extends Person
+{
+	public function __construct($name = 'unknow', $surname = 'unknow', $id = 0, $topic = 'IT')
+	{
+		parent::__construct($name, $surname);
+		$this->id = $id;
+		$this->topic = $topic;
+	}
+
+	public function identify()
+	{
+		echo "<p>My stdent id is <stroing>$this->id</strong>";
+		echo "and I am studying <strong>$this->topic</strong></p>";
+	}
+}
+
+$b = new Student('DURU' , 'hapDev', 15020, 'PHP');
+
+$b->identify();
+
+$b->introduce();
+
+?>
