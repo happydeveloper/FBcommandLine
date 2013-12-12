4ae073db2ccd343c248c0f774c365bac5e68dc67
DURU <enfn2001@gmail.com>
2013-12-12 11:29:43 +0900

    데이타베이스 연결 테스트 및 쿼리 테스트

--- index.php
+++ index.php
@@ -29,6 +29,34 @@ $app->map('/comment', function() use($app) {
 	$app->render('ot_comment.php');
 })->via('GET', 'POST');
 
+$app->get('/dbtest', 'getStream');
+
 $app->run();
 
+function getConnection()
+{
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "1111";
+	$dbname = "fb_archive";
+	//$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8", $dbuser, $dbpass,
+                    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   	return $dbh;
+}
+
+function getStream() {
+    $sql = "select * from stream";
+    try {
+        $db = getConnection();
+        $stmt = $db->query($sql);
+        $stream = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo '{"stream": ' . json_encode($stream) . '}';
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
 ?>
