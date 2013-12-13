07907923be9ce64108f7c50c41866af203d242d4
DURU <enfn2001@gmail.com>
2013-12-13 18:07:48 +0900

    insert TEST

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ $app->map('/comment', function() use($app) {
 
 $app->get('/dbtest', 'getStream');
 
+$app->get('/dbinsertTest','pushStream');
 $app->run();
 
 function getConnection()
@@ -59,4 +60,28 @@ function getStream() {
         echo '{"error":{"text":'. $e->getMessage() .'}}';
     }
 }
+
+	function pushStream() {
+	  	//$request = Slim::getInstance()->request();
+ 	 	//$stream = json_decode($request->getBody());
+  		$sql = "insert into stream(message, message_tags, permalink, source_id, created_time, created, post_id, filter_key) values ('message', 'message_tag', 'permalik', 'source_id', NOW(), NOW(), 'post_id', 'filter_key');";
+  		try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);
+	/*	$stmt->bindParam("message", $wine->name);
+		$stmt->bindParam("message_tags", $wine->grapes);
+		$stmt->bindParam("permalink", $wine->country);
+		$stmt->bindParam("source_id", $wine->region);
+		$stmt->bindParam("created_time", $wine->year);
+		$stmt->bindParam("created", 
+	*/	
+		$stmt->execute();
+		$stmt->id = $db->lastInsertId();
+		$db = null;
+		//	echo json_encode($stream);
+		} catch(PDOException $e) {
+			echo '{"error":{"text":'. $e->getMessage() .'}}';
+		}
+	}
+
 ?>
