a0f7f1d60edcca2aaa58fed07337e51eb8c65fdc
DURU <enfn2001@gmail.com>
2013-12-19 23:42:56 +0900

    불필요한 줄 삭제 FQL 클래스

--- classes/fqlmanager.php
+++ classes/fqlmanager.php
@@ -7,7 +7,6 @@ class fqlManager
 	{
 		$this->fqlList = array();
 		$this->fqlList['GROUPS_WALL'] = "SELECT post_id, created_time, permalink, message, actor_id, message_tags, like_info, comment_info FROM stream WHERE source_id = ";
-		//$this->fqlList['GROUPS_WALL'] = "SELECT post_id, created_time, permalink, message FROM stream WHERE source_id = ";
 		$this->fqlList['COMMENT'] = "SELECT fromid, username, text, time, post_id FROM comment WHERE post_id = ";
 		$this->fqlList['MY_FRIENDS']= "SELECT uid, pic, pic_square, name FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = ";
 	}
