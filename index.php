105c154dad7e6da3865bdcf33aeaf92ee7887b83
DURU <enfn2001@gmail.com>
2013-12-27 16:19:28 +0900

    stream 스케쥴 돌리기 테스트

--- index.php
+++ index.php
@@ -81,8 +81,8 @@ function pushStream() {
 			echo "<a href=\"<?php echo 'common\/logout.php'; ?>\">logout</a>";
 			
 		
-for($i = 0; $i < 365; $i++)
-{			$baseDate = "2012-12-29";
+for($i = 0; $i < 1093; $i++)
+{			$baseDate = "2010-12-31";
 //echo date('Y-m-d', strtotime($baseDate. ' + '.$i.' days')).'<br />';
 $loadDate = date('Y-m-d', strtotime($baseDate. ' + '.$i.' days'));
 echo $loadDate.'<br />';
