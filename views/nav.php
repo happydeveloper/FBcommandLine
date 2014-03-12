8a173ac93155609a63580555b0653cda9fe2fa3d
DURU <enfn2001@gmail.com>
2014-03-12 16:50:08 +0900

    기능 추가 리스트 작성

--- views/nav.php
+++ views/nav.php
@@ -6,9 +6,14 @@
 	$url="http://ucloud.duru.pe.kr";
 ?>
                         <li><a href=<?php echo $url."/fcmd/friends" ?> >친구들 얼굴들</a></li>
+<li><a href="#">현재 뉴스피드</a></li>
+<li><a href="#">타임머신기능</a></li>
+<hr />
                         <li><a href=<?php echo $url."/fcmd/codingeverybody" ?> >생활코딩</a></li>
 
                         <li><a href=<?php echo $url."/fcmd/engfordev" ?> >개발자영어</a></li>
+<hr />
+			<li><a href='#'>나눙에 보기 기능 - 북마크</a></li>
                 </ul>
         </div>
 </nav>
