2267bdbc8c5534bd4c368dd4984122c7d649f96e
dev <enfn2001@gmail.com>
2014-04-29 10:42:51 +0900

    로컬환경에서 테스트

--- tests.sh
+++ tests.sh
@@ -1,2 +1,2 @@
- #vendor/phpunit/phpunit/phpunit.php --stderr --bootstrap test/bootstrap.php test/tests.php
+ vendor/phpunit/phpunit/phpunit.php --stderr --bootstrap test/bootstrap.php test/tests.php
  vendor/phpunit/phpunit/phpunit.php --stderr --bootstrap test/bootstrap.php test/MeasureCntTestCase.php
