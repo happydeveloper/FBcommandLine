6131236661394be520f5d1e2a825b8b98c8ded7c
DURU <enfn2001@gmail.com>
2014-06-14 22:39:53 +0900

    태경이네 집에서  디버깅

--- views/myfriends.php
+++ views/myfriends.php
@@ -86,6 +86,13 @@ if($basetaskfacebook->user) {
 	else {
 		echo "<span>얼굴책 명령어 라인</span>";
 	}
+
+	if($basetaskfacebook->user){
+		var_dump($basetaskfacebook->user);
+
+        	echo  fqlManager::loadFql('MY_FRIENDS').$basetaskfacebook->user;
+	}
+
     ?>
   </body>
 
