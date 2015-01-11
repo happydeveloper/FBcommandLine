3793acc015b5795723f6de37bcabbe0914b18332
DURU <enfn2001@gmail.com>
2015-01-11 14:45:12 +0900

    google analysistics add

--- views/_head.php
+++ views/_head.php
@@ -32,3 +32,13 @@ background-color:#444;color:#F0F8FF;margin-left:45px; border-bottom: 1px solid #
     <title> 페이스북 그룹 글 </title>
   </head>
   <body>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58453511-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
