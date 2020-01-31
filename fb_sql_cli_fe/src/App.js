fe0cb02773ac43ed8bf51ddb8f7a87c455e95a79
duru <enfn2001@gmail.com>
2020-01-31 13:24:58 +0900

    프론트앤드 대문 페이지 컴포넌트 생성

---
+++ fb_sql_cli_fe/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>facebook sql and cli</h2>
+      <input value="facebook auth" type="submit" />
+    </div>
+  );
+}
+
+export default App;
